a790e1b70f420c5f4d9e78bddf0be5699490a6a1
ttouchard-ensae <tristan.touchard@ensae.fr>
2024-01-28 21:15:29 +0000

    Atttempt to code the Solver function : failure to call Grid inside Solver : doen't work

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -1,12 +1,10 @@
 from grid import Grid
+from solver import Solver
 
 g = Grid(2, 3)
 print(g)
 
-data_path = "../input/"
-file_name = data_path + "grid0.in"
+print(g.is_sorted())
+print(g.n)
 
-print(file_name)
-
-g = Grid.grid_from_file(file_name)
-print(g)
+print(g.get_solution())
--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,10 +1,9 @@
-from grid import Grid
-
 class Solver(): 
     """
     A solver class, to be implemented.
     """
-    
+    from grid import Grid
+
     def get_solution(self):
         """
         Solves the grid and returns the sequence of swaps at the format 
@@ -12,5 +11,27 @@ class Solver():
         """
         # TODO: implement this function (and remove the line "raise NotImplementedError").
         # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
-        raise NotImplementedError
+        
+        from grid import Grid
+        compteur = 0
+        list_of_moves = []
+        while self.is_sorted() is False and compteur < m*n : 
+            for i in range (self.n) :
+                for j in range (self.m-1) :
+                    if self.state[i][j] > self.state[i][j+1] : 
+                        self.swap(self,(i,j),(i,j+1))
+                        list_of_moves.append(((i,j),(i,j+1)))
+            # On trie chacune des lignes dans l'ordre croissant : vrai si while se répète suffisamment de fois (m*n) au max ? 
+            compteur = compteur + 1 
+
+        compteur = 0 
+        while self.is_sorted() is False and compteur < m*n :     
+            for i in range (self.m) :
+                for j in range (self.n) : 
+                    if self.state[j][i] > self.state[j+1][i] :
+                        self.swap(self,(j,i),(j,i+1))
+                        list_of_moves.append(((i,j),(i,j+1)))
+            compteur = compteur + 1
+        
+        return(list_of_moves, self)
 
