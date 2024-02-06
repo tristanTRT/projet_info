1821bdbb2eaa31c2ecbb8b46b3fa9c86d9fc259a
tristanTRT <48414993+tristanTRT@users.noreply.github.com>
2024-02-06 22:16:47 +0100

    Représentation graphique main.py

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -1,26 +1,43 @@
 from grid import Grid
 from solver import Solver
+from graph import Graph 
+import matplotlib.pyplot as plt
 
-data_path = "/home/onyxia/work/projet_info/input/"
-file_name = data_path + "grid0.in"
+data_path = "/workspaces/projet_info/input/"
+file_name = data_path + "grid4.in"
 
 
 
 
 g = Grid.grid_from_file(file_name)
-print(g)# vaut pour représentation graphique de la question 3
+
+
+a = Grid(1,1)
+
+a.afficher_grille(g)
+
 
 s = Solver()
 
-print(s.get_solution(g))
+print(s.get_solution(g)) # affiche la grille initiale
 
-from graph import Graph 
 
-graph1 = Graph.graph_from_file(data_path + "graph1.in")
+
+s.afficher_grille(g) # affiche la grille résolue
+
+
+
+
+graph1 = Graph.graph_from_file(data_path + "graph2.in")
 print(graph1) # le fichier donne le nombre de noeuds et le nombres de ponts au total / il décrit ensuite les arrangements faits entre les ponts
 
 # But de l'implementation initiale de BFS : trouver plus court chemin entre noeud 1 et 2 par exemple 
 
 h = Graph()
 
-print(h.bfs(graph1,1,3))
+print(h.bfs(graph1, 2,3), 'resultat')
+
+
+
+
+
